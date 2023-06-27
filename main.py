83cbe9cbe93079c3bd2c26cc687d3ae34a20c524
Arthur <solowjow@mail.upb.de>
2023-06-27 20:51:18 +0200

    added clustering and outlier detection

--- main.py
+++ main.py
@@ -1,8 +1,61 @@
-import pandas as pd
 from sklearn import svm
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+random.seed(10)
+
+def find_cluster_outliers(data):
+    """
+    Find outliers for each cluster in the data.
+
+    Parameters:
+    - data: pandas DataFrame containing the data
+    - cluster_labels: pandas Series or array-like object containing the cluster labels
+    - value_column: column name representing the values to analyze for outliers
+
+    Returns:
+    - Dictionary with cluster labels as keys and a list of outlier values as values
+    """
+    # Create a dictionary to store outlier values for each cluster
+    outliers = {}
+    cluster_labels = data['Cluster']
+    value_column = data['MV (EUR).1']
+    player_column = data['PLAYER']
+    unique_clusters = np.unique(cluster_labels)
+
+    for cluster in unique_clusters:
+        # Select values for the current cluster
+        cluster_values = data[data['Cluster'] == cluster]['MV (EUR).1']
+        cluster_players = data[data['Cluster'] == cluster]['PLAYER']
+
+        # Calculate quartiles and interquartile range (IQR)
+        q1 = np.percentile(cluster_values, 25)
+        q3 = np.percentile(cluster_values, 75)
+        iqr = q3 - q1
+
+        # Define the upper and lower bounds for outliers
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+
+        # Find outliers for the current cluster
+        cluster_outliers = []
+        for value, player in zip(cluster_values, cluster_players):
+            if value < lower_bound or value > upper_bound:
+                outlier_dict = {"player": player, "MV (EUR).1": value}
+                cluster_outliers.append(outlier_dict)
+
+        # Store outliers in the dictionary
+        outliers[cluster] = cluster_outliers
+
+    return outliers
+
 
 numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 
@@ -12,31 +65,59 @@ player_data.dropna(inplace=True, axis=0)
 
 
 
-
 drop_cols = ['NUMBER', 'ROLE', 'MV (EUR)', 'PLAYER']
 #print(player_data.columns)
 player_data_prep = player_data.drop(drop_cols, axis=1)
 
-X = player_data.select_dtypes(include=numerics)
-print(X)
+categorical_columns = player_data_prep.select_dtypes(include=['object']).columns
+numerical_columns = player_data_prep.select_dtypes(include=['float64', 'int64']).columns
 
+# Dummy code categorical columns
+dummy_coded_data = pd.get_dummies(player_data_prep, columns=categorical_columns)
+
+# Scale numerical columns
 scaler = StandardScaler()
-scaled_features = pd.DataFrame(scaler.fit_transform(X))
+scaled_data = pd.DataFrame(scaler.fit_transform(player_data_prep[numerical_columns]), columns=numerical_columns)
 
-dummies = pd.get_dummies(player_data_prep)
+# Combine scaled numerical columns and dummy coded columns
+final_data = pd.concat([scaled_data, dummy_coded_data], axis=1)
 
-n_clusters = 5
-n_samples = 300
-epochs = 100
+# Remove duplicate columns
+final_data = final_data.loc[:, ~final_data.columns.duplicated()]
 
-kmeans = KMeans(n_clusters=n_clusters, random_state=0, max_iter=epochs)
-y_kmeans = kmeans.fit_predict(X)
+# Define the pipeline
+pipeline = Pipeline([
+    ('clusterer', KMeans(n_clusters=5))  # Perform clustering with K-means
+])
 
-plt.scatter(X[y_kmeans==0, 0], X[y_kmeans==0, 1], s=100, c='red', label ='Cluster 1')
-#plt.scatter(X[y_kmeans==1, 0], X[y_kmeans==1, 1], s=100, c='blue', label ='Cluster 2')
-#plt.scatter(X[y_kmeans==2, 0], X[y_kmeans==2, 1], s=100, c='green', label ='Cluster 3')
-#plt.scatter(X[y_kmeans==3, 0], X[y_kmeans==3, 1], s=100, c='cyan', label ='Cluster 4')
-#plt.scatter(X[y_kmeans==4, 0], X[y_kmeans==4, 1], s=100, c='magenta', label ='Cluster 5')
-#Plot the centroid. This time we're going to use the cluster centres  #attribute that returns here the coordinates of the centroid.
-#plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label = 'Centroids')
+# Fit the pipeline to the final data
+pipeline.fit(final_data)
+
+# Get the cluster labels
+clusters = pipeline['clusterer'].labels_
+
+
+# Create a DataFrame with the "value" column and cluster labels
+df = pd.DataFrame({'MV (EUR).1': player_data_prep['MV (EUR).1'], 'Cluster': clusters,
+                   'PLAYER': player_data['PLAYER']})
+
+
+
+big_df = pd.concat([df, player_data_prep], axis=1)
+
+
+# Create the boxplot using matplotlib
+plt.boxplot([df[df['Cluster'] == i]['MV (EUR).1'] for i in df['Cluster'].unique()],
+            labels=['Cluster {}'.format(i) for i in df['Cluster'].unique()])
+plt.xlabel('Cluster')
+plt.ylabel('MV (EUR).1')
+plt.title('Boxplot of Value by Cluster')
 plt.show()
+
+
+# Specify the column name representing the values and find outliers
+outliers = find_cluster_outliers(df)
+
+# Print the outliers for each cluster
+for cluster, outlier_values in outliers.items():
+    print(f"Cluster {cluster} outliers: {outlier_values}")
